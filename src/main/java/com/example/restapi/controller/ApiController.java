e18d1edb11e5ed98b78f75fca19f8735c3ad7206
foldoverr <stiskindanil@gmail.com>
2024-11-12 23:36:13 +0300

    Update v0.14

--- src/main/java/com/example/restapi/controller/ApiController.java
+++ src/main/java/com/example/restapi/controller/ApiController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.*;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -16,9 +17,49 @@ import java.util.concurrent.TimeUnit;
 @Validated
 public class ApiController {
 
+    private static final String URL = "jdbc:postgresql://localhost:5432/mydatabase";
+    private static final String USER = "myuser";
+    private static final String PASSWORD = "mypassword";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
     @GetMapping("/static")
-    public ResponseEntity<User> getStaticJson() {
-        User user = new User(); // Создаем пользователя с login = "admin" и password = "admin"
+    public ResponseEntity<User> getStaticJson(@RequestParam String login) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        User user = null;
+        try{
+            connection = getConnection();
+            statement = connection.createStatement();
+            String query = "SELECT u.login, u.password, e.email, u.date " +
+                    "FROM user_login u LEFT JOIN user_email e ON u.login = e.login " +
+                    "WHERE u.login = '" + login + "'";
+            resultSet = statement.executeQuery(query);
+            if (resultSet.next()) {
+                String password = resultSet.getString("password");
+                String email = resultSet.getString("email");
+                Timestamp date = resultSet.getTimestamp("date");
+                user = new User(login, password, date, email);
+            }
+            if (user == null) {
+                throw new UserNotFoundException("User with login '" + login + "' not found");
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) resultSet.close();
+                if (statement != null) statement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
         requestLatency();
         return ResponseEntity.ok(user);
     }
@@ -26,12 +67,14 @@ public class ApiController {
     // POST-запрос с использованием Map и ручной проверки
     @PostMapping("/login/map")
     public ResponseEntity<?> loginWithMap(@RequestBody Map<String, String> request) {
-        if (request.size() > 2) {
+        if (request.size() != 4) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Request contains unexpected fields");
         }
 
         String login = request.get("login");
         String password = request.get("password");
+        String email = request.get("email");
+        Timestamp date = Timestamp.valueOf(request.get("date"));
 
         if (login == null || login.isBlank()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Login cannot be blank");
@@ -45,9 +88,25 @@ public class ApiController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Password cannot be blank");
         }
 
-        User user = new User(login, password);
+        User user = new User(login, password, date, email);
+        String insertUserLogin = "INSERT INTO user_login (login, password, date) VALUES (?, ?, ?); \n INSERT INTO user_email (login, email) VALUES (?, ?);";
+        int rowsUpdated = 0;
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(insertUserLogin)) {
+            connection.setAutoCommit(false);
+            statement.setString(1, user.getLogin());
+            statement.setString(2, user.getPassword());
+            statement.setTimestamp(3, user.getDate());
+            statement.setString(4, user.getLogin());
+            statement.setString(5, user.getEmail());
+            rowsUpdated = statement.executeUpdate();
+            connection.commit();  // Завершаем транзакцию
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         requestLatency();
-        return ResponseEntity.ok(user);
+        return ResponseEntity.ok(rowsUpdated);
     }
 
     // POST-запрос с использованием валидации
@@ -57,10 +116,18 @@ public class ApiController {
         return ResponseEntity.ok(user);
     }
 
+
+
     // Обработчик ошибок валидации
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<String> handleValidationExceptions(ConstraintViolationException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Validation error: " + ex.getMessage());
+    }
+
+    public class UserNotFoundException extends RuntimeException {
+        public UserNotFoundException(String message) {
+            super(message);
+        }
     }
 
     public void requestLatency(){
--- src/main/java/com/example/restapi/model/User.java
+++ src/main/java/com/example/restapi/model/User.java
@@ -2,8 +2,11 @@ package com.example.restapi.model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+
+import java.sql.Time;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.sql.Timestamp;
 
 public class User {
     @NotBlank(message = "Login cannot be blank")
@@ -13,23 +16,37 @@ public class User {
     @NotBlank(message = "Password cannot be blank")
     private String password = null;
 
-    private String date = null;
+    private Timestamp date = null;
+
+    @NotBlank(message = "Login cannot be blank")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\\\.[A-Za-z]{2,6}$", message = "Email wrong")
+    private String email = null;
 
     // Конструктор для GET-запроса
     public User() {
         this.login = "admin";
         this.password = "admin";
-        this.date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        //this.date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        this.email = "admin@example.com";
     }
 
     // Конструктор для POST-запроса
     public User(String login, String password) {
         this.login = login;
         this.password = password;
-        this.date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        //this.date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        this.email = "admin@example.com";
+    }
+
+    public User(String login, String password, Timestamp date, String email) {
+        this.login = login;
+        this.password = password;
+        this.date = date;
+        this.email = email;
     }
 
     public String getLogin() { return login; }
     public String getPassword() { return password; }
-    public String getDate() { return date; }
+    public Timestamp getDate() { return date; }
+    public String getEmail() { return email; }
 }
